faef2a9cc9d057a651fdf155bb1b3a29cda05c28
jpotts <jeffpotts01@gmail.com>
2017-04-25 15:18:52 +0200

    Add "make shared" end point which will convert a local rule to a shared rule.

---
+++ rule-management-repo/src/main/java/com/metaversant/alfresco/webscripts/ConvertToSharedRule.java
@@ -0,0 +1,120 @@
+package com.metaversant.alfresco.webscripts;
+
+import com.metaversant.alfresco.rules.Utilities;
+import org.alfresco.model.ContentModel;
+import org.alfresco.service.cmr.repository.ChildAssociationRef;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.cmr.search.SearchService;
+import org.alfresco.service.namespace.NamespaceService;
+import org.alfresco.service.namespace.QName;
+import org.apache.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.extensions.webscripts.Cache;
+import org.springframework.extensions.webscripts.DeclarativeWebScript;
+import org.springframework.extensions.webscripts.Status;
+import org.springframework.extensions.webscripts.WebScriptRequest;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.metaversant.alfresco.rules.Utilities.getSharedRulesFolder;
+import static org.alfresco.events.types.DataType.Date;
+
+/**
+ * This web script will turn a local rule into a shared rule.
+ *
+ * Created by jpotts on 1/19/17.
+ */
+public class ConvertToSharedRule extends DeclarativeWebScript {
+
+    private NodeService nodeService;
+    private SearchService searchService;
+
+    private Logger logger = Logger.getLogger(ConvertToSharedRule.class);
+
+    @Override
+    protected Map<String, Object> executeImpl(WebScriptRequest req, Status status, Cache cache) {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("message", "");
+
+        Object content = req.parseContent();
+        if (!(content instanceof JSONObject)) {
+            status.setCode(Status.STATUS_BAD_REQUEST);
+            status.setRedirect(true);
+            return model;
+        }
+
+        JSONObject ruleInfo = (JSONObject) content;
+
+        // check for required Data
+        String sourceNodeRefString = null;
+        try {
+            if (ruleInfo.getString("nodeRef") == null) {
+                status.setCode(400, "Rule folder nodeRef is required");
+                status.setRedirect(true);
+                return model;
+            } else {
+                sourceNodeRefString = ruleInfo.getString("nodeRef");
+            }
+        } catch (JSONException je) {
+            status.setCode(500, "Problem parsing JSON: " + je.getMessage());
+            status.setRedirect(true);
+            return model;
+        }
+        logger.debug("Converting to shared: " + sourceNodeRefString);
+
+        // convert the nodeRef argument to an actual nodeRef
+        NodeRef sourceNodeRef = new NodeRef(sourceNodeRefString);
+        if (!nodeService.exists(sourceNodeRef)) {
+            status.setCode(404, "Source nodeRef not found: " + sourceNodeRefString);
+            status.setRedirect(true);
+            return model;
+        }
+
+        // grab the shared rules folder
+        NodeRef sharedRulesFolder = Utilities.getSharedRulesFolder(searchService);
+        logger.debug("Got shared rules folder");
+
+        // get the name of the source folder and use that as the name of the shared rule set
+        String sourceFolderName = (String) nodeService.getProperty(sourceNodeRef, ContentModel.PROP_NAME);
+
+        // TODO: Need to catch the case when this folder name already exists
+
+        Map<QName, Serializable> props = new HashMap<QName, Serializable>();
+        props.put(ContentModel.PROP_NAME, sourceFolderName);
+
+        ChildAssociationRef childRef = nodeService.createNode(
+                sharedRulesFolder,
+                ContentModel.ASSOC_CONTAINS,
+                QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, sourceFolderName),
+                ContentModel.TYPE_FOLDER,
+                props
+        );
+        NodeRef ruleTarget = childRef.getChildRef();
+        logger.debug("Created new folder in Shared Rules for move");
+
+        Utilities.moveRules(nodeService, sourceNodeRef, ruleTarget);
+        logger.debug("Move complete");
+
+        // Create a link from the new Shared Rule to the existing rule folder
+        Utilities.linkToRuleFolder(nodeService, ruleTarget, sourceNodeRef);
+        logger.debug("Link complete");
+
+        return model;
+    }
+
+    public void setNodeService(NodeService nodeService) {
+        this.nodeService = nodeService;
+    }
+
+    public void setSearchService(SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+}
